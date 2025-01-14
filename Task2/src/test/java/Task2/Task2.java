a4ac3ca446cc4ab0908f9a5bd7934b966b822af8
sonsonoquendo <jefferson.oquendo09@gmail.com>
2025-01-14 17:03:00 +0800

    Add files via upload

--- Task2/src/test/java/Task2/Task2.java
+++ Task2/src/test/java/Task2/Task2.java
@@ -1,13 +1,14 @@
 package Task2;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.ByteArrayInputStream;
 import java.time.Duration;
 import java.util.List;
 import org.testng.Assert;
@@ -15,17 +16,19 @@ import org.testng.annotations.Test;
 
 public class Task2 {
 
+    private static WebDriver driver;
+
     @Test
     public void AmaysimTest() throws InterruptedException {
         // Set the path to the ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
 
         // Initialize the ChromeDriver with the options
-        WebDriver driver = new ChromeDriver();
+        this.driver = new ChromeDriver();
 
         Actions action = new Actions(driver);
         JavascriptExecutor jse = (JavascriptExecutor) driver;
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         // Maximize the browser window
         driver.manage().window().maximize();
@@ -33,20 +36,26 @@ public class Task2 {
         // Open a website
         driver.get("https://www.amaysim.com.au/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Screenshot();
 
         // Your test code here
         String title = driver.getTitle();
         System.out.print(title);
 
         driver.findElement(By.xpath("(//span[contains(text(),'SIM plans')])")).click();
-        Thread.sleep(5000);
+//        Thread.sleep(5000);
+        Screenshot();
 
+        Thread.sleep(5000);
         driver.findElement(By.xpath("(//a[@class='btn no-icon btn-orange'][normalize-space()='Buy now'])[11]")).click();
+        Screenshot();
 
         Thread.sleep(15000);
         driver.findElement(By.xpath("//span[@class='css-15xa8x'][normalize-space()='pick a new number']")).click();
         driver.findElement(By.xpath("//div[@class='css-1vy9u2m']//div[@class='css-ikyri5']//*[name()='svg']//*[name()='path' and contains(@d,'M8.97358 2')]")).click();
         Thread.sleep(3000);
+        Screenshot();
+
         driver.findElement(By.xpath("(//span[@class='css-1tisfka'])")).click();
 
         Thread.sleep(5000);
@@ -59,6 +68,7 @@ public class Task2 {
         driver.findElement(By.xpath("(//input[@id='field-input--9'])")).sendKeys("0412345678");
         driver.findElement(By.xpath("(//input[@id='field-input--16'])")).sendKeys("Level 6, 17-19 Bridge St, SYDNEY NSW 2000");
         driver.findElement(By.xpath("(//li[@id='react-autowhatever-1--item-0'])")).click();
+        Screenshot();
 
         WebElement paymentHeader = driver.findElement(By.id("payments-header-name"));
         jse.executeScript("arguments[0].scrollIntoView(true);", paymentHeader);
@@ -69,16 +79,26 @@ public class Task2 {
         driver.findElement(By.xpath("(//input[@id='Field-expiryInput'])")).sendKeys("0127");
         driver.findElement(By.xpath("(//input[@id='Field-cvcInput'])")).sendKeys("123");
         driver.switchTo().defaultContent();
+        Screenshot();
 
         driver.findElement(By.xpath("//div[@class='css-c5g9lg']//div[@class='css-1417z9a']//*[name()='svg']")).click();
         driver.findElement(By.xpath("//button[normalize-space()='pay now']")).click();
+        Screenshot();
 
         //Assert - Credit Card payment failed
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Your attempt to pay via Credit Card has failed.')]")));
         WebElement ErrorMessage = driver.findElement(By.xpath("//span[contains(text(),'Your attempt to pay via Credit Card has failed.')]"));
         String ExpectedErrorMessage = "Your attempt to pay via Credit Card has failed. Please ensure you have enough funds and try again or use another credit card.";
-        Assert.assertEquals(ErrorMessage.getText(),ExpectedErrorMessage);
+        Screenshot();
+        Assert.assertEquals(ErrorMessage.getText(), ExpectedErrorMessage);
         System.out.println(ErrorMessage.getText());
         System.out.println(ExpectedErrorMessage);
+
+    }
+
+    public static void Screenshot() {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment("Screenshot", new ByteArrayInputStream(screenshot));
 
         // Close the browser
         //driver.quit();
